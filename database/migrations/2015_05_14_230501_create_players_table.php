fb99c1e8f8e36f7a0b273a071589933b77aac8f3
jrb467 <jrb467@cornell.edu>
2015-05-14 19:05:05 -0400

    cleanup!

---
+++ database/migrations/2015_05_14_230501_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePlayersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('players', function(Blueprint $table)
+		{
+			$table->string('name', 50);
+			$table->timestamps();
+			$table->string('email', 50)->unique('email');
+			$table->string('password', 128);
+			$table->integer('id', true);
+			$table->string('remember_token', 100)->nullable();
+			$table->boolean('is_admin')->default(0);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('players');
+	}
+
+}
---
+++ database/migrations/2015_05_14_230502_add_foreign_keys_to_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToEventsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('events', function(Blueprint $table)
+		{
+			$table->foreign('game_name', 'events_ibfk_1')->references('name')->on('games')->onUpdate('CASCADE')->onDelete('CASCADE');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('events', function(Blueprint $table)
+		{
+			$table->dropForeign('events_ibfk_1');
+		});
+	}
+
+}
